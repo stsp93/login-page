05c98695c3dbdd464c3fd76fe4ad43431dbebffd
stsp93 <stspsp@gmail.com>
2023-08-04 18:47:09 +0300

    label initial check

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+
+// label effect
+
+$('input').each(function(i, el) {  //initial check
+    if(el.value !== '') {
+        el.previousElementSibling.classList.add('not-empty')
+
+    }
+}) 
+
+$('input').on('focusout', function() {
+    if($(this).val() === '') {
+        $(this).prev().removeClass('not-empty')
+    }
+})
+
 $('input').on('focus', function() {
     $(this).prev().addClass('not-empty')
 });
 
-$('.toggle-form').click(function() {
+
+// Login/Register slide logic
+$('.toggle-form').on('click', function() {
     $('.register-form, .login-form').toggleClass('active');
     $('.register-form').toggleClass('hidden-right');
     $('.login-form').toggleClass('hidden-left');
