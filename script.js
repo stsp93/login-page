f7f15a188bd6072e429c2cc0adc062fefcc04c51
stsp93 <stspsp@gmail.com>
2023-08-04 20:26:07 +0300

    button ripple effect added

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 
 // label effect
 
-$('input').each(function(i, el) {  //initial check
-    if(el.value !== '') {
+$('input').each(function (i, el) {  //initial check
+    if (el.value !== '') {
         el.previousElementSibling.classList.add('not-empty')
 
     }
-}) 
+})
 
-$('input').on('focusout', function() {
-    if($(this).val() === '') {
+$('input').on('focusout', function () {
+    if ($(this).val() === '') {
         $(this).prev().removeClass('not-empty')
     }
 })
 
-$('input').on('focus', function() {
+$('input').on('focus', function () {
     $(this).prev().addClass('not-empty')
 });
 
 
 // Login/Register slide logic
-$('.toggle-form').on('click', function() {
+$('.toggle-form').on('click', function () {
     $('.register-form, .login-form').toggleClass('active');
     $('.register-form').toggleClass('hidden-right');
     $('.login-form').toggleClass('hidden-left');
-  });
+});
+
+
+$('button').on('click', function (e) {
+    let x = e.clientX - this.getBoundingClientRect().x;
+    let y = e.clientY - this.getBoundingClientRect().y; 
+
+
+    console.log(this.getBoundingClientRect().x );
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+        ripples.remove()
+    }, 1000);
+});
