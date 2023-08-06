a40261a67a9e8390e440b248e8a15dfc0e0525a5
stsp93 <stspsp@gmail.com>
2023-08-06 17:34:04 +0300

    accordion added and refactor

--- src/homePage/homePage.js
+++ src/homePage/homePage.js
@@ -1,7 +1,7 @@
 function createStar() {
     $('.header').append($('<div></div>').css(
         {left: `${Math.random() * 100}vw`,
-        animationDuration: `${Math.random() * 30 + 10}s`
+        animationDuration: `${Math.random() * 30 +5}s`
     }
     ).addClass('star'));
 }
@@ -11,3 +11,5 @@ const numberOfStars = 500;
 for (let i = 0; i < numberOfStars; i++) {
     setTimeout(createStar, Math.random() * 100000 )
 }
+
+$('#accordion').accordion();
